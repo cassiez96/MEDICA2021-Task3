03e8f99cee9026a569ccfba141a5e443cb6a97d6
jb <jeanbenoit.delbrouck@gmail.com>
2021-02-02 16:58:40 +0100

    jb

--- version2_nmtpytorch/split_input_target.py
+++ version2_nmtpytorch/split_input_target.py
@@ -8,12 +8,18 @@ from torchvision import transforms
 import torch
 import numpy as np
 from tqdm import tqdm
+import glob
 
 MIMIC_IMAGES_DIR = 'images'
+INDIANA_IMAGES_DIR = 'indiana-images'
 OUTPUT_DIR = 'out_nmtpytorch'
 
 
-def load_images(subject_id, study_id):
+class Indiana:
+    images = glob.glob(INDIANA_IMAGES_DIR + "/*")
+
+
+def load_images_mimic(subject_id, study_id):
     d = os.path.join(
         MIMIC_IMAGES_DIR,
         'p' + str(subject_id)[:2],  # 10000032 -> p10
@@ -21,6 +27,11 @@ def load_images(subject_id, study_id):
         's' + str(study_id)
     )
     return [os.path.join(d, f) for f in os.listdir(d)]
+
+
+def load_images_indiana(study_id):
+    s = 'CXR' + str(study_id) + '_'
+    return [image for image in Indiana.images if s in image]
 
 
 def write_to_file(file, text, replicate=1):
@@ -43,8 +54,11 @@ if __name__ == "__main__":
     # create dir if not exist
     output_dir = OUTPUT_DIR
     os.makedirs(output_dir, exist_ok=True)
-    i = 0
-    for split in ['train', 'dev']:
+
+    for split in ['train', 'dev', 'indiana_dev']:
+        feature_file = os.path.join(output_dir, split + '_avgpool.npy')
+        if os.path.exists(feature_file):
+            continue
         f_findings = open(os.path.join(output_dir, "{}.findings".format(split)), "w")
         f_bg_findings = open(os.path.join(output_dir, "{}.bg_and_findings".format(split)), "w")
         f_impression = open(os.path.join(output_dir, "{}.impression").format(split), "w")
@@ -58,17 +72,27 @@ if __name__ == "__main__":
             subject_id = report["subject_id"]
             study_id = report["study_id"]
 
-            # connect report with corresponding images in the same order
-            images_for_report = load_images(subject_id, study_id)
-            for image in images_for_report:
-                img = preprocess(Image.open(image).convert('RGB'))
-                with torch.no_grad():
-                    out = layer_getter(img.unsqueeze(0).cuda())
-                    feat = out['my_features'].squeeze()
-                    features.append(feat.cpu().data.numpy())
+            # get images
+            if 'indiana' in split:
+                images_for_report = load_images_indiana(study_id)
+            else:
+                images_for_report = load_images_mimic(subject_id, study_id)
 
+            # get features
+            if len(images_for_report) == 0:
+                features.append(np.zeros(2048))
+            else:
+                for image in images_for_report:
+                    img = preprocess(Image.open(image).convert('RGB'))
+                    with torch.no_grad():
+                        out = layer_getter(img.unsqueeze(0).cuda())
+                        feat = out['my_features'].squeeze()
+                        features.append(feat.cpu().data.numpy())
+
+            # write samples
             write_to_file(f_findings, findings, replicate=len(images_for_report))
             write_to_file(f_bg_findings, f"BACKGROUND: {bg} FINDINGS: {findings}", replicate=len(images_for_report))
             write_to_file(f_impression, impression, replicate=len(images_for_report))
             write_to_file(f_ids, f"{study_id} {subject_id}", replicate=len(images_for_report))
-        np.save(os.path.join(output_dir, split + '_avgpool'), np.array(features))
+
+        np.save(feature_file, np.array(features))
